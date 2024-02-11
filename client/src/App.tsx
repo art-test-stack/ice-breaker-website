7534edb83255358bc396e872c9ad81987c6bd052
Elin Skånlund Berntsen <elin.berntsen@orbitntnu.com>
2024-02-11 15:47:49 +0100

    Remove: default vite and react start page

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -6,21 +6,13 @@ import { LoginButton } from './LoginRegisterMenu/LoginButton.tsx'
 import LoginMenu from './LoginRegisterMenu/LoginMenu.tsx'
 import { Search } from './SearchBar/Search.tsx'
 
+
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
       <Search/>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
@@ -29,11 +21,10 @@ function App() {
           Edit <code>src/App.tsx</code> and save to test HMR
         </p>
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+
       {/* <div><LoginButton /></div> */}
       <div> <LoginMenu /> </div>
+
     </>
   )
 }
