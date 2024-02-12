ce9597ec10e5717d4ae9b508a3b7f49c896b8e93
Joy Soerbotten <jhsorbot@ntnu.no>
2024-02-12 18:43:40 +0100

    new file: GameInfo.tsx and GameInfo.css

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -3,12 +3,14 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 import GameDescription from './GameDescription/GameDescription'
+import GameInfo from './GameInfo/GameInfo'
 
 function App() {
   const [count, setCount] = useState(0)  
   return (
     <>
       <div>
+        <GameInfo numPlayers='1-6' duration='5 hours' equipments={['mom','dad','sister','brother', 'niece']} categories='this, that, those'/>
         <GameDescription title="Game Name" gameText='Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum. Enim eu turpis egestas pretium aenean pharetra magna ac. Quis varius quam quisque id diam vel quam elementum. Aliquet enim tortor at auctor. Eu tincidunt tortor aliquam nulla facilisi cras. Enim diam vulputate ut pharetra sit amet aliquam. Dictum varius duis at consectetur lorem donec massa. Netus et malesuada fames ac turpis egestas integer. Elementum facilisis leo vel fringilla est ullamcorper. Magna etiam tempor orci eu lobortis elementum nibh tellus molestie. Dolor sit amet consectetur adipiscing. A lacus vestibulum sed arcu non odio. Adipiscing vitae proin sagittis nisl rhoncus mattis rhoncus urna neque. Dictumst vestibulum rhoncus est pellentesque. Vel turpis nunc eget lorem dolor sed. Nunc sed blandit libero volutpat!'/> 
         <a href="https://vitejs.dev" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />
--- client/src/GameDescription/GameDescription.tsx
+++ client/src/GameDescription/GameDescription.tsx
@@ -1,4 +1,3 @@
-import React, {useState} from 'react';
 import './GameDescription.css';
 
 /* <GameDescription title='' gameText='' */
