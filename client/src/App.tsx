92301654f78185de157708b75a42c696fdaee329
Ola Haldor Hådi <olahaldorhadi@dhcp-10-22-13-13.wlan.ntnu.no>
2024-02-14 12:54:15 +0100

    feat: game grid
    add better organisation of gamecard and game page in app.tsx
    
    Issue: #1
    
    task: create list component for gameCards

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -15,37 +15,38 @@ function App() {
   }
 
   return (
-    <>
-    
-      <div className="gameBox">
-          <GameGrid games={[{
-          imgSrc: './assets/cards.webp',
-          imgAlt: 'Image 1',
-          title: 'Game 1',
-          category: 'Action',
-        },
-        {
-          imgSrc: 'image2.jpg',
-          imgAlt: 'Image 2',
-          title: 'Game 2',
-          category: 'Card',
-        },
-        {
-          imgSrc: 'image3.jpg',
-          imgAlt: 'Image 3',
-          title: 'Game 3',
-          category: 'Adventure',
-        },
-        {
-          imgSrc: 'image3.jpg',
-          imgAlt: 'Image 4',
-          title: 'Game 4',
-          category: 'Adventure',
-        }]}></GameGrid>
-
-        <GamePage title={gameExample.title} gameText={gameExample.description} numPlayers = {gameExample.numPlayers}
-        duration={gameExample.duration} equipments={gameExample.equipments} categories={gameExample.categories}/>
+    <><div className ="gameSection">
+          <div className="gameBox">
+              <GameGrid games={[{
+              imgSrc: './assets/cards.webp',
+              imgAlt: 'Image 1',
+              title: 'Game 1',
+              category: 'Action',
+            },
+            {
+              imgSrc: 'image2.jpg',
+              imgAlt: 'Image 2',
+              title: 'Game 2',
+              category: 'Card',
+            },
+            {
+              imgSrc: 'image3.jpg',
+              imgAlt: 'Image 3',
+              title: 'Game 3',
+              category: 'Adventure',
+            },
+            {
+              imgSrc: 'image3.jpg',
+              imgAlt: 'Image 4',
+              title: 'Game 4',
+              category: 'Adventure',
+            }]}></GameGrid>
+          </div>
+        <div>
+          <GamePage title={gameExample.title} gameText={gameExample.description} numPlayers = {gameExample.numPlayers}
+          duration={gameExample.duration} equipments={gameExample.equipments} categories={gameExample.categories}/>
         </div>
+      </div>
   </>
   )
 }
