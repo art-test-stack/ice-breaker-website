b7b7ebe5d0d7ba489f9cd62f999037a59d41f449
Joy Soerbotten <jhsorbot@ntnu.no>
2024-02-14 11:01:02 +0100

    modified and added files. GamePage now includes both both game description and additional game informations 	modified:   App.tsx 	modified:   GameDescription.css 	modified:   GameInfo.css 	new file:   GamePage.css 	new file:   GamePage.tsx

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -2,16 +2,27 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
-import GameDescription from './GameDescription/GameDescription'
-import GameInfo from './GameInfo/GameInfo'
+import GamePage from './GamePage/GamePage.tsx'
 
 function App() {
-  const [count, setCount] = useState(0)  
+  const [count, setCount] = useState(0)
+
+  // Just an example. Not sure ab the datatypes, but should they shouldn't be that hard to change
+  const gameExample = {
+    title: 'GameName',
+    description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum. Enim eu turpis egestas pretium aenean pharetra magna ac. Quis varius quam quisque id diam vel quam elementum. Aliquet enim tortor at auctor. Eu tincidunt tortor aliquam nulla facilisi cras. Enim diam vulputate ut pharetra sit amet aliquam. Dictum varius duis at consectetur lorem donec massa. Netus et malesuada fames ac turpis egestas integer. Elementum facilisis leo vel fringilla est ullamcorper. Magna etiam tempor orci eu lobortis elementum nibh tellus molestie. Dolor sit amet consectetur adipiscing. A lacus vestibulum sed arcu non odio. Adipiscing vitae proin sagittis nisl rhoncus mattis rhoncus urna neque. Dictumst vestibulum rhoncus est pellentesque. Vel turpis nunc eget lorem dolor sed. Nunc sed blandit libero volutpat!',
+    numPlayers: '1-6',
+    duration: '1-5 hours',
+    equipments: ['dice','timer','paper'],
+    categories: 'dice game'
+  }
+
   return (
     <>
       <div>
-        <GameInfo numPlayers='1-6' duration='5 hours' equipments={['mom','dad','sister','brother', 'niece']} categories='this, that, those'/>
-        <GameDescription title="Game Name" gameText='Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum. Enim eu turpis egestas pretium aenean pharetra magna ac. Quis varius quam quisque id diam vel quam elementum. Aliquet enim tortor at auctor. Eu tincidunt tortor aliquam nulla facilisi cras. Enim diam vulputate ut pharetra sit amet aliquam. Dictum varius duis at consectetur lorem donec massa. Netus et malesuada fames ac turpis egestas integer. Elementum facilisis leo vel fringilla est ullamcorper. Magna etiam tempor orci eu lobortis elementum nibh tellus molestie. Dolor sit amet consectetur adipiscing. A lacus vestibulum sed arcu non odio. Adipiscing vitae proin sagittis nisl rhoncus mattis rhoncus urna neque. Dictumst vestibulum rhoncus est pellentesque. Vel turpis nunc eget lorem dolor sed. Nunc sed blandit libero volutpat!'/> 
+        <GamePage title={gameExample.title} gameText={gameExample.description} numPlayers = {gameExample.numPlayers}
+        duration={gameExample.duration} equipments={gameExample.equipments} categories={gameExample.categories}/>
+
         <a href="https://vitejs.dev" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />
         </a>
---
+++ client/src/GamePage/GamePage.tsx
@@ -0,0 +1,23 @@
+import GameInfo from "../GameInfo/GameInfo"
+import GameDescription from "../GameDescription/GameDescription"
+import './GamePage.css'
+
+interface Props{
+    title: string;
+    gameText: string;
+    numPlayers: string;
+    duration: string;
+    equipments: string[];
+    categories: string;
+}
+
+function GameDescriptionAndAdditionalInfo({title, gameText, numPlayers, duration, equipments, categories}: Props) {
+    return (
+        <div className="GamePage">
+            <GameDescription title={title} gameText={gameText}/>
+            <GameInfo numPlayers={numPlayers} duration={duration} equipments={equipments} categories={categories}/>
+        </div>
+    );
+}
+
+export default GameDescriptionAndAdditionalInfo;
