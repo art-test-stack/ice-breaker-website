ae1127d6642d9756cc733d548f66e624f75da223
Elin Skånlund Berntsen <elin.berntsen@orbitntnu.com>
2024-02-19 21:04:54 +0100

    remove: popup wrapper

--- client/src/CreateGamePage/PublishButton/PublishButton.tsx
+++ client/src/CreateGamePage/PublishButton/PublishButton.tsx
@@ -1,17 +1,12 @@
 import "./PublishButton.css"
-import Popup from 'reactjs-popup';
-import 'reactjs-popup/dist/index.css';
-
-
 
 const PublishButton: React.FC<{onClick: () => void}> = ({ onClick}) => {
     return (
         <div>
-            <Popup trigger={<button onClick={onClick} id="publishButton">
-                Publish</button>} position='bottom center'>
-                <div className="popUp">Game successfully added!</div>
-            </Popup>
-        </div>
+        <button onClick={onClick} id="publishButton" >
+            <span style={{ display: 'inline-block' }}>Publish</span>
+            </button>
+    </div>
     ); 
 }
 
