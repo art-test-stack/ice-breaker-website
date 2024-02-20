ddf60e504c91b057f60f4b47ac65caf4204c4e99
Ola Haldor <olahh@ntnu.stud.no>
2024-02-20 21:30:34 +0100

    feat: react routing
    commit to merge with main
    
    Issue: #43

--- client/src/pages/CreateGamePage.tsx
+++ client/src/pages/CreateGamePage.tsx
@@ -1,17 +1,49 @@
 import GoBack from "../components/atoms/GoBack";
-import PublishButton from "../CreateGamePage/PublishButton/PublishButton.tsx";
-import GameNameField from "../CreateGamePage/GameNameField/GameNameField";
-import DescriptionPrompt from "../CreateGamePage/DescriptionPrompt/DescriptionPrompt";
-import AddEquipment from "../CreateGamePage/AddEquipment/AddEquipment";
+import '../CreateGamePage/CreateGamePage.css';
+import '../goBackWrapper.css';
+import PublishButton from "../CreateGamePage/PublishButton/PublishButton";
+import GameNameField, {gameName} from "../CreateGamePage/GameNameField/GameNameField";
+import DescriptionPrompt, {gameDescription} from "../CreateGamePage/DescriptionPrompt/DescriptionPrompt";
+import AddEquipment, {equipment} from "../CreateGamePage/AddEquipment/AddEquipment";
+import CreateGameButton from "../CreateGamePage/CreateGameButton/CreateGameButton";
+import { AddCategoryDropdown, activeCategoriesOutput } from "../CreateGamePage/AddCategory/AddCategoryDropdown";
+import PlayerNoSlider, {minPlayers, maxPlayers} from "../CreateGamePage/SelectPlayerNumberSlider/PlayerNoSlider";
 import LoginMenu from '../LoginRegisterMenu/LoginMenu.tsx';
 import { AuthUI, CurrentUserDataProvider } from "../firebase/auth";
 import { Title } from '../Title/Title.tsx';
 import '../CreateGamePage/CreateGamePage.css';
 import '../goBackWrapper.css';
 import '../App.css';
+import { database } from '../firebase/init';
+import { push, ref } from "@firebase/database";
+
+const publishButtonClicked = () => {
+    /*if (gameName == ''){
+        window.alert("All fields must be filled")*/
+    
+    if (gameName == '' || gameDescription == '') {
+        window.alert("All fields must be filled!")
+    } else {
+        push(ref(database, 'games'), {
+            name: gameName,
+            description: gameDescription,
+            minPlayers: minPlayers,
+            equipment: equipment,
+            categories: activeCategoriesOutput,
+            maxPlayers: maxPlayers,
+        }).then(() => {
+            window.alert("The game " + gameName + " was added successfully!")
+            window.location.reload() 
+        }).catch((error) => {
+            window.alert("Error adding game: " + error.message)
+        })
+    }
+
+}
+
 
 export function CreateGamePage() {
-  return (
+    return (
     <div>
         <div id='header'> 
           <div id='titleContainer'>
@@ -27,25 +59,32 @@ export function CreateGamePage() {
           {/* <div><LoginButton /></div> */}
         </div>
         <div className="createGamePageContent">
-          <div className="goBackWrapper">
-              <GoBack onClick={() => {
-                  console.log('go back button clicked');
-              }}/>
-              <div id="createGameBox">
-                  <div id="createGameInputBox">
-                      <GameNameField/>
-                      <DescriptionPrompt/>
-                  </div>
-                  <div id="createGameRightContainer">
-                      <div id="createGameSettingsBox">
-                          <AddEquipment/>
-                      </div>
-                      <PublishButton onClick={function (): void {
-                            throw new Error("Function not implemented.");
-                        } }></PublishButton>
-                  </div>
-              </div>
-          </div>
+    <div>
+        <div className="goBackWrapper">
+            <GoBack onClick={() => {
+                console.log('go back button clicked');
+            }}/>
+            <div id="createGameBox">
+                <div id="createGameInputBox">
+                    <GameNameField/>
+                    <DescriptionPrompt/>
+                </div>
+                <div id="createGameRightContainer">
+
+                    <div id="createGameSettingsBox">
+                        <AddEquipment/>
+                        <PlayerNoSlider/>
+                        <AddCategoryDropdown/>
+                    </div>
+
+                    <CreateGameButton onClick={() => {
+                        console.log('create game button clicked');
+                    }}/>
+                        <PublishButton onClick={publishButtonClicked}/>
+                    </div>
+                </div>
+            </div>
+        </div>
         </div>
     </div>
   );
