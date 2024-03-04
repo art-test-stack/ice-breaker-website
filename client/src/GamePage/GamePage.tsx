3c100c6ddfca4be4cdd8c7cc9bc49691cb13a53d
Elin Skånlund Berntsen <elin.berntsen@orbitntnu.com>
2024-03-04 19:12:35 +0100

    add: average score for every game

--- client/src/GamePage/GamePage.tsx
+++ client/src/GamePage/GamePage.tsx
@@ -2,9 +2,12 @@ import GameInfo from "../GameInfo/GameInfo"
 import GameDescription from "../GameDescription/GameDescription"
 import './GamePage.css'
 import ReviewSaveShareButton from "./ReviewSaveShare/reviewSaveShareButton";
-import ReviewComponent from "./ReviewComponent/ReviewComponent";
 import ReviewsList from "./ReviewComponent/ReviewsList";
 import { CurrentGameReviewsProvider } from "../firebase/reviewProvider";
+import { useContext } from "react";
+import { currentReviewsList } from "../firebase/reviewProvider";
+import AverageReviewScore from "./ReviewComponent/AverageReviewScore";
+
 
 interface Props{
     title: string;
@@ -16,25 +19,40 @@ interface Props{
     author: string;
     gameId: string;
 }
+interface Review {
+    username: string; 
+    comment: string; 
+    rating: string; 
+}
+
+
 
 function GameDescriptionAndAdditionalInfo({title, gameText, numPlayers, duration, equipments, categories, author, gameId}: Props) {
-    const userNames = ['Test_user1', 'Test_user2', 'Test_user3'];
-    const review = ['Lorem ipsum', 
-    'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.', 
-    '"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."'];
+
+ 
 
     return ( 
         <>
         <div className="GamePageWrap">
             <div className="GamePage">
                 <GameDescription title={title} gameText={gameText}/> 
-                <CurrentGameReviewsProvider gameId={gameId}>    
+                <CurrentGameReviewsProvider gameId={gameId}>  
+                <div className="reviewScoresSpan">
+                    <h2>Reviews</h2>  
+                    <div className="averageReviewScore">
+                    <AverageReviewScore/>
+                    </div>
+                    
+                </div>
+
                 <ul className="everyReview">
-                    <h2>Reviews</h2>
                     <ReviewsList/>
                 </ul>
+
+                                
                 </CurrentGameReviewsProvider>
             </div>
+     
             <div className="Right-Section-GameInfo">
                 <GameInfo numPlayers={numPlayers} duration={duration} equipments={equipments} categories={categories} author={author}/>
                 <ReviewSaveShareButton/>  
---
+++ client/src/GamePage/ReviewComponent/AverageReviewScore.tsx
@@ -0,0 +1,40 @@
+import { useContext } from "react";
+import { currentReviewsList } from "../../firebase/reviewProvider";
+
+interface Review {
+    username: string; 
+    comment: string; 
+    rating: string; 
+}
+
+function AverageScore(){
+    const reviewsList = useContext(currentReviewsList);
+    let array_test: number[] = []
+    console.log(reviewsList);
+    
+    reviewsList && Object.values(reviewsList).map((review: Review, index: number) => (
+        array_test[index] = parseInt(review.rating)
+    ))
+
+    let getAverage = 0;
+    
+    for (let i = 0; i < array_test.length; i++){
+        getAverage+= array_test[i];
+    }
+    getAverage = getAverage / array_test.length;
+
+    return Math.round(getAverage).toFixed(2)
+}
+
+
+export default function AverageReviewScore() {
+
+    return <>
+        {
+            
+        }
+        <div style={{margin:'0px'}}>
+            {AverageScore()}
+        </div>
+    </>
+}
