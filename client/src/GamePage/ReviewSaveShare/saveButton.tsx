f3faf23695fd017a98da48294fc0878fc013b983
Arthur Testard <testardarthur@gmail.com>
2024-03-04 14:03:47 +0100

    commit back up

--- client/src/GamePage/ReviewSaveShare/saveButton.tsx
+++ client/src/GamePage/ReviewSaveShare/saveButton.tsx
@@ -1,14 +1,46 @@
-import { useState } from 'react';
+import { useContext, useState } from 'react';
 import { Button } from '@mui/material';
 import FavoriteIcon from '@mui/icons-material/Favorite';
+import { push, ref } from 'firebase/database';
+import { database } from '../../firebase/init';
+import { currentUserData } from '../../firebase/auth';
+import { useLocation } from 'react-router-dom';
 
+type saveOnPlaylistProps = {
+  userId: string | undefined,
+  gameId: string
+} 
+const saveOnDatabase = ({ userId, gameId}: saveOnPlaylistProps) => {
+
+  console.log(gameId)
+  // Push reviews to database
+  push(ref(database, 'playlists/save/' + userId + '/' + gameId)).then((res) => {
+      // push reviewIDs to games.
+      console.log("here")
+      window.alert('Game added to favorite !')
+  }).catch((error) => {
+      console.log('Error: ', error)
+      window.alert("Error adding review: " + error.message)
+  })
+}
+
+const deleteFromDataBase = () => {
+  window.alert("Game deleted from favorite")
+}
 
 export const SaveButton = () => {
   const [gameSaved, setGameSaved] = useState(false)
+  const currentLocation = useLocation();
+  const gameId = currentLocation.pathname.split("/")[2]
+
+  const userData = useContext(currentUserData)
+  const userId = userData?.data?.username
+
   const colorComp = {color: gameSaved ? "red" : "white"}
 
   const handleClick = () => {
-      window.alert(!gameSaved ? 'Game added to favorite !' : "Game deleted from favorite")
+      !gameSaved ? saveOnDatabase({userId: userId, gameId: gameId}) : deleteFromDataBase()
+      // window.alert(!gameSaved ? 'Game added to favorite !' : "Game deleted from favorite")
       setGameSaved(!gameSaved)
   }
   
