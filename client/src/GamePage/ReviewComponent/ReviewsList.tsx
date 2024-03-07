29c91122f0c558999ac264481e8b02ca1dff33e3
Spu7Nix <augusea@stud.ntnu.no>
2024-03-07 05:10:47 +0100

    feat: quirky message when there are no reviews yet on a game

--- client/src/GamePage/ReviewComponent/ReviewsList.tsx
+++ client/src/GamePage/ReviewComponent/ReviewsList.tsx
@@ -10,16 +10,19 @@ interface Review {
 
 export default function ReviewsList() {
     const reviewsList = useContext(currentReviewsList);
-    console.log(reviewsList);
-    return <>
-        {
-            reviewsList && Object.values(reviewsList).map((review: Review, index: number) => (
-                <ReviewComponent
-                    key={index}
-                    userName={review.username}
-                    givenReview={review.comment}
-                    rating={review.rating}            />
-            ))
-        }
-    </>
+    if (!reviewsList || Object.values(reviewsList).length === 0) {
+        return <i style={{color: "#aaaaaa", fontFamily: "Calibri, sans-serif", fontSize: "24px"}}> No reviews... yet...</i>
+    } else {
+        return <>
+            {
+                Object.values(reviewsList).map((review: Review, index: number) => (
+                    <ReviewComponent
+                        key={index}
+                        userName={review.username}
+                        givenReview={review.comment}
+                        rating={review.rating}            />
+                ))
+            }
+        </>
+    }
 }
