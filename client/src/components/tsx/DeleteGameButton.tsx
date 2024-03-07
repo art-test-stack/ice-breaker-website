7923302058dff6f686c44740338e5948832eaef3
Joy Soerbotten <jhsorbot@ntnu.no>
2024-03-07 16:30:33 +0100

    fix: animated button
    still needs fixing such that button animation is completed before confirm pops up

--- client/src/components/tsx/DeleteGameButton.tsx
+++ client/src/components/tsx/DeleteGameButton.tsx
@@ -1,29 +1,32 @@
 import '../css/DeleteGameButton.css'
 import * as React from 'react'
-import Button from '@mui/material/Button'
-import DeleteIcon from '@mui/icons-material/Delete'
-import SendIcon from '@mui/icons-material/Send'
-import Stack from '@mui/material/Stack'
 import { currentUserData } from "../../firebase/auth"
-import { useNavigate } from 'react-router-dom'
+import { currentGamesList } from '../../firebase/gameprovider'
+import { useNavigate, useLocation } from 'react-router-dom'
+import { push, ref } from 'firebase/database'
+import { database } from '../../firebase/init'
+import { BorderLeft } from '@mui/icons-material'
 
 const DeleteGameButton: React.FC<{onClick: () => void}> = ({onClick}) => {
     const navigate = useNavigate()
-    
+    const gameList = React.useContext(currentGamesList)
     const clicked = () => {
         var del = confirm('Are you sure you want to delete this game?')
         if(del){
             console.log('deleted')
+            // find game from the firebase
+
+            // delete game from the firebase
             navigate ('/') // navigates back to home
         }
     }
     return (
         <div>
-        <Button onClick={clicked} variant="outlined" startIcon={<DeleteIcon />} color='error' size='large' id="deleteGameButton">
-        Delete game
-        </Button>
-    </div>
+            <button onClick={clicked} id='deleteGameButton'>
+                <span>Delete Game</span>
+            </button>
+        </div>
     ); 
 }
 
-export default DeleteGameButton;
+export default DeleteGameButton
