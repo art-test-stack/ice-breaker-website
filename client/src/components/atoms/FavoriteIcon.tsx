6a1f4eb2715a4b83297b20ce3756fe2ec647567b
Vegard Kolaas <Vegard.Kolaas@gmail.com>
2024-03-07 14:57:35 +0100

    feat: Added skeleton logic for toggling favorite icons on gamecards

--- client/src/components/atoms/FavoriteIcon.tsx
+++ client/src/components/atoms/FavoriteIcon.tsx
@@ -1,10 +1,18 @@
 import { MdFavorite } from "react-icons/md";
-
-// import { MdOutlineFavoriteBorder } from "react-icons/md";
+import { MdOutlineFavoriteBorder } from "react-icons/md";
 
 export default function FavoriteButton(){    
     function FavoriteIcon(){
-        return(<MdFavorite/>)
+        const isFavorite = true; //Placeholder as favorites have not been added to databases at the time of writing.
+        // TODO: Replace with database request when possible
+        if (isFavorite){
+            return(<MdFavorite/>)
+        }
+        else{
+            return(<MdOutlineFavoriteBorder/>)
+        }
+        
+        
     }
 
     return(
