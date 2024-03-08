8968cf881a15ba92d53fafe69b20e76167bfbd93
Joy Soerbotten <jhsorbot@ntnu.no>
2024-03-08 13:08:01 +0100

    fix: can hide button

--- client/src/components/tsx/DeleteGameButton.tsx
+++ client/src/components/tsx/DeleteGameButton.tsx
@@ -4,28 +4,32 @@ import { useNavigate, useLocation } from 'react-router-dom'
 import { ref, remove, getDatabase } from 'firebase/database'
 // import { useContext } from 'react'
 
-function showButton(){
-    // if user or admin logged in
-    // if user made the game -> should be able to delete game
-
-}
 
 const DeleteGameButton: React.FC<{onClick: () => void}> = ({onClick}) => {
     const navigate = useNavigate()
     const path = useLocation().pathname // url of page
     const db = getDatabase()    // gets the database
-    const dbRef = ref(db, path) // makes database reference for game page
+    const dbRef = ref(db, path) // makes database reference for game
+
+    // if delete button is clicked, the game is deleted from database 
+    // and we return to homepage
     const clicked = () => {
-        var del = confirm('Are you sure you want to delete this game?')
+        
+        const del = confirm('Are you sure you want to delete this game?\nThis action is permanent.')
         if(del){
             remove(dbRef).then(() => console.log('Deleted'))    // removes reference from database
             .catch((error) => console.log('Error occured when deleting: ' + error.message)) // catches error if not deleted
-            navigate ('/') // navigates back to home
+            navigate ('/') // navigates back to home when game is deleted
         }
     }
+
+    // if author of game is user or admin -> button shows
+    // user.key = game.creator or user.key = userData.key.moderator
+    
+    var canDelete = false
     return (
         <div>
-            <button onClick={clicked} id='deleteGameButton'>
+            <button onClick={clicked} id='deleteGameButton' className={canDelete ? 'hide': ''}>
                 <span>Delete Game</span>
             </button>
         </div>
