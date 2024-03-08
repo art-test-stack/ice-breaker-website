467b59f7ae2cb757242eac5d34c9deb85df9164b
Joy Soerbotten <jhsorbot@ntnu.no>
2024-03-08 12:47:31 +0100

    fix: delete button functional

--- client/src/components/tsx/DeleteGameButton.tsx
+++ client/src/components/tsx/DeleteGameButton.tsx
@@ -1,21 +1,25 @@
 import '../css/DeleteGameButton.css'
-import * as React from 'react'
 import { currentUserData } from "../../firebase/auth"
-import { getGame, currentGamesList } from '../../firebase/gameprovider'
 import { useNavigate, useLocation } from 'react-router-dom'
-import { push, ref } from 'firebase/database'
-import { database } from '../../firebase/init'
-import { BorderLeft } from '@mui/icons-material'
+import { ref, remove, getDatabase } from 'firebase/database'
+// import { useContext } from 'react'
+
+function showButton(){
+    // if user or admin logged in
+    // if user made the game -> should be able to delete game
+
+}
 
 const DeleteGameButton: React.FC<{onClick: () => void}> = ({onClick}) => {
     const navigate = useNavigate()
+    const path = useLocation().pathname // url of page
+    const db = getDatabase()    // gets the database
+    const dbRef = ref(db, path) // makes database reference for game page
     const clicked = () => {
         var del = confirm('Are you sure you want to delete this game?')
         if(del){
-            console.log('deleted')
-            // find game from the firebase
-
-            // delete game from the firebase
+            remove(dbRef).then(() => console.log('Deleted'))    // removes reference from database
+            .catch((error) => console.log('Error occured when deleting: ' + error.message)) // catches error if not deleted
             navigate ('/') // navigates back to home
         }
     }
